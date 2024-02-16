49dfc4190bc8cf024a10c179c3d43631d5050844
Dulran Sam <127298642+DulranSam@users.noreply.github.com>
2024-02-17 00:44:30 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -47,7 +47,7 @@ app.use(
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://127.0.0.1:5173"], //react project
+    origin: ["http://localhost:5173", "http://127.0.0.1:5173","https://films-frontend.vercel.app/"], //react project
   })
 );
 
