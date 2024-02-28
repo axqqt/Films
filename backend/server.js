48ba3e74a44b4843e9efbd7d587df33d1be1e761
Veloxal <dulransamarasinghe4@gmail.com>
2024-02-28 21:57:38 +0530

    minor change to get vercel working

--- backend/server.js
+++ backend/server.js
@@ -57,7 +57,7 @@ function authenticated(req,res,next){
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://127.0.0.1:5173","https://films-frontend.vercel.app/"], //react project
+    origin: ["http://localhost:5173", "http://127.0.0.1:5173","https://films-frontend.vercel.app"], //react project
   })
 );
 
